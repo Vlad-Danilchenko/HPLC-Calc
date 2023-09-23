195e1c9c7de479160d24e3137792b71537163728
Freasy-Vlad <104465507+Vlad-Danilchenko@users.noreply.github.com>
2023-09-23 16:19:55 +0300

    add fix modal

--- src/components/BurgerMenu/BurgerMenu.jsx
+++ src/components/BurgerMenu/BurgerMenu.jsx
@@ -1,27 +1,23 @@
 import { useState } from "react";
-import Modal from "../Modal/Modal"
-import burgerMenuImg from '../../assets/img/burgerMenuImg.svg'
+import Modal from "../Modal/Modal";
+import burgerMenuImg from "../../assets/img/burgerMenuImg.svg";
 
 import { Img } from "./BurgerMenu.styled";
 
 const BurgerMenu = () => {
-    const [isOpen, setIsOpen] = useState(false)
-    
-    const handleClic = (e) => {
-        const isClick = e.target;
-        if (isClick === e.currentTarget) {
-            setIsOpen(!isOpen)
-        }
-    }
-    return (
-        <div>
-       
-            {/* <button type="button" onClick={handleClic}> */}
-            <Img onClick={handleClic} src={burgerMenuImg} />
-            {/* </button> */}
-            {isOpen && <Modal isOpen={handleClic} />}
-        </div>
-    )
-}
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClic = () => {
+    setIsOpen((state) => !state);
+  };
+  return (
+    <div>
+      {/* <button type="button" onClick={handleClic}> */}
+      <Img onClick={handleClic} src={burgerMenuImg} />
+      {/* </button> */}
+      {isOpen && <Modal isOpen={handleClic} />}
+    </div>
+  );
+};
 
 export default BurgerMenu;
